9bb75cc786e1688b5192074f74c942406a4cf7cc
sarvesh <007sarveshsingh@gmail.com>
2018-03-26 19:33:20 +0530

    Background setting

--- app/src/main/java/com/example/sarvesh/i_turnout/Home.java
+++ app/src/main/java/com/example/sarvesh/i_turnout/Home.java
@@ -2,13 +2,18 @@ package com.example.sarvesh.i_turnout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tv= (TextView) findViewById(R.id.textview);
+        tv.setSelected(true);
 
     }
 }
