b8536172a2c7463a663690bc79185b0026b2feea
sarvesh <007sarveshsingh@gmail.com>
2018-03-27 22:06:01 +0530

    Background setting

--- app/src/main/java/com/example/sarvesh/i_turnout/Home.java
+++ app/src/main/java/com/example/sarvesh/i_turnout/Home.java
@@ -1,12 +1,16 @@
 package com.example.sarvesh.i_turnout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
     TextView tv;
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,34 @@ public class Home extends AppCompatActivity {
         tv= (TextView) findViewById(R.id.textview);
         tv.setSelected(true);
 
+    }
+    /*public void teacher(View v)
+    {
+        Intent in=new Intent(Home.this, Student.class);
+        startActivity(in);
+    }*/
+    public void signin(View v)
+    {
+        Intent in;
+        EditText e=(EditText) findViewById(R.id.editText);
+        String s=e.getText().toString();
+        if(s.length()<1)
+        {
+            in = new Intent(Home.this, Home.class);
+        }
+        else if(s.charAt(0)=='a'){
+            in = new Intent(Home.this, Admin.class);
+        }
+        else if (s.charAt(0)=='t'){
+            in = new Intent(Home.this, Teacher.class);
+        }
+        else if(s.charAt(0)=='B' || s.charAt(0)=='b' || s.charAt(0)=='M' || s.charAt(0)=='m'){
+            in = new Intent(Home.this, Student.class);
+        }
+        else
+        {
+            in = new Intent(Home.this, Home.class);
+        }
+        startActivity(in);
     }
 }
